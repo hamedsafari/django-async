3979169ca51e65ffed9e62349ebac1da1eb50a8b
Hamed Safari <hamed.safari1990@gmail.com>
2024-05-03 12:43:23 +0330

    used axios to read stream data.

--- frontend/pages/stream_sync.tsx
+++ frontend/pages/stream_sync.tsx
@@ -1,9 +1,24 @@
+import {useEffect} from "react";
+import axios from "axios";
+
 export default function SyncAPI() {
-  return (
-    <main>
-      <div>
-        <p>Hello World!</p>
-      </div>
-    </main>
-  )
+    const readStream = async () => {
+        const response = await axios.get("http://localhost:8080/async/chat/", {responseType: "stream"});
+        const stream = response.data;
+        stream.on("data", data => {
+            data = data.toString();
+            console.log(data);
+        })
+    }
+    useEffect(() => {
+        readStream();
+    }, [])
+
+    return (
+        <main>
+            <div>
+                <p>Hello World!</p>
+            </div>
+        </main>
+    )
 }
