2ad12df975ab132de866b71a910bb51a9d3298a1
Hamed Safari <hamed.safari1990@gmail.com>
2024-05-05 21:35:38 +0330

    improved nginx config to disable uwsgi buffering.

--- backend/apps/chat/urls.py
+++ backend/apps/chat/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from apps.chat.views import content
+from apps.chat.views import sync_content, async_content
 
-urlpatterns = [path("", content, name="content")]
+urlpatterns = [
+    path("sync-content", sync_content, name="sync_content"),
+    path("async-content", async_content, name="async_content")
+]
--- backend/apps/chat/views.py
+++ backend/apps/chat/views.py
@@ -1,13 +1,26 @@
+import time
 import random
 import asyncio
 from django.http import StreamingHttpResponse
 
+MESSAGE = "This is a sample message.\n"
 
-async def content_stream():
-    for char in "Hello World!":
+
+def sync_content_stream():
+    for char in MESSAGE:
+        yield char
+        time.sleep(random.random())
+
+
+def sync_content(request):
+    return StreamingHttpResponse(sync_content_stream())
+
+
+async def async_content_stream():
+    for char in MESSAGE:
         yield char
         await asyncio.sleep(random.random())
 
 
-async def content(request):
-    return StreamingHttpResponse(content_stream())
+async def async_content(request):
+    return StreamingHttpResponse(async_content_stream())
